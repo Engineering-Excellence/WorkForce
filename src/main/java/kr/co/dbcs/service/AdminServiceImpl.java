b0f32d48258d780515085c7726a9b04187a414f3
Jaejin Lee <jaejin1112@gmail.com>
2023-05-18 19:28:25 +0900

    [make administrator view]

--- src/main/java/kr/co/dbcs/service/AdminServiceImpl.java
+++ src/main/java/kr/co/dbcs/service/AdminServiceImpl.java
@@ -7,6 +7,7 @@ import java.sql.*;
 
 import kr.co.dbcs.domain.EmpDTO;
 
+import static kr.co.dbcs.util.JdbcManager.BR;
 import static kr.co.dbcs.util.JdbcManager.BW;
 import static kr.co.dbcs.util.JdbcManager.MANAGER;
 
@@ -26,26 +27,55 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public void adminMenu() throws IOException {
     	while(true) {
-    		BW.write("\n======================================================================\n");
-    		BW.write("|\t\t     임직원근태관리 관리자 메뉴\t\t\t     |\n");
-    		BW.write("======================================================================\n");
-    		BW.write("|\t    1. 직원관리\t\t   |\t        2. 출근관리\t     |\n");
-    		BW.write("======================================================================\n");
-    		BW.write("|\t    3. 휴가관리\t\t   |\t        4. 급여관리\t     |\n");
-    		BW.write("======================================================================\n");
-    		BW.write("|\t    5. 부서관리\t\t   |\t        6. 직급관리\t     |\n");
-    		BW.write("======================================================================\n");
-    		BW.write("|\t\t원하는 기능을 선택하세요.(0번 : 종료)\t\t     |\n");
-            BW.write("======================================================================\n");
-    		BW.flush();
+    		showMenu();
     		
+    		String menu = BR.readLine().trim();
     		
+    		switch (menu) {
+            case "0":
+                BW.write("홈 화면으로 돌아갑니다.\n");
+                BW.flush();
+                return;
+            case "1":
+            	// 직원관리
+                break;
+            case "2":
+                // 출근관리
+                break;
+            case "3":
+                // 휴가관리
+                break;
+            case "4":
+                // 급여관리
+                break;
+            case "5":
+                // 부서관리
+                break;
+            case "6":
+                // 직급관리
+                break;
+            default:
+                BW.write("잘못된 입력입니다.\n"); 
+                BW.flush();
+                break;
+    		}
     	}
     }
 
 	@Override
-	public void showAdminInfo() {
-		
+	public void showMenu() throws IOException {
+		BW.write("\n======================================================================\n");
+		BW.write("|\t\t     임직원근태관리 관리자 메뉴\t\t\t     |\n");
+		BW.write("======================================================================\n");
+		BW.write("|\t    1. 직원관리\t\t   |\t        2. 출근관리\t     |\n");
+		BW.write("======================================================================\n");
+		BW.write("|\t    3. 휴가관리\t\t   |\t        4. 급여관리\t     |\n");
+		BW.write("======================================================================\n");
+		BW.write("|\t    5. 부서관리\t\t   |\t        6. 직급관리\t     |\n");
+		BW.write("======================================================================\n");
+		BW.write("|\t\t원하는 기능을 선택하세요.(0번 : 종료)\t\t     |\n");
+        BW.write("======================================================================\n");
+		BW.flush();
 	}
     
 }
