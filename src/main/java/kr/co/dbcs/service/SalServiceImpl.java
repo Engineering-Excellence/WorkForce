254f715732e70d45a1c77ccf98c4b17d7a1c6c3f
Jaejin Lee <jaejin1112@gmail.com>
2023-05-18 21:03:16 +0900

    [make SalService & Impl & before pull main]

---
+++ src/main/java/kr/co/dbcs/service/SalServiceImpl.java
@@ -0,0 +1,17 @@
+package kr.co.dbcs.service;
+
+import java.sql.*;
+
+import static kr.co.dbcs.util.JdbcManager.BR;
+import static kr.co.dbcs.util.JdbcManager.BW;
+import static kr.co.dbcs.util.JdbcManager.MANAGER;
+
+public class SalServiceImpl implements SalService{
+	private final Connection conn = MANAGER.getConnection();
+    private final Statement stmt = MANAGER.getStatement();
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+    
+    public SalServiceImpl() throws SQLException {
+    }
+}
