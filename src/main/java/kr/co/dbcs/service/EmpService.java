1ff5c99b34878e24d0f56e121e7adb17e239efd2
Kyle <prophet@hanyang.ac.kr>
2023-05-18 15:02:15 +0900

    [Update Singleton Pattern]

--- src/main/java/kr/co/dbcs/service/EmpService.java
+++ src/main/java/kr/co/dbcs/service/EmpService.java
@@ -1,10 +1,11 @@
 package kr.co.dbcs.service;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public interface EmpService {
 
-    void empMenu() throws IOException;
-    void showEmpInfo();
+    void empMenu() throws IOException, SQLException, ClassNotFoundException;
+    void showEmpInfo() throws SQLException, ClassNotFoundException, IOException;
 
 }
