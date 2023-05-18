e249d831d69de7e855886c1fe9efe481d6d36020
Jaejin Lee <jaejin1112@gmail.com>
2023-05-18 16:55:13 +0900

    [commit before pull main]

--- src/main/java/kr/co/dbcs/service/AdminServiceImpl.java
+++ src/main/java/kr/co/dbcs/service/AdminServiceImpl.java
@@ -2,8 +2,12 @@ package kr.co.dbcs.service;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.sql.*;
 
+import kr.co.dbcs.domain.EmpDTO;
+
+import static kr.co.dbcs.util.JdbcManager.BW;
 import static kr.co.dbcs.util.JdbcManager.MANAGER;
 
 @Slf4j
@@ -13,13 +17,30 @@ public class AdminServiceImpl implements AdminService {
     private final Statement stmt = MANAGER.getStatement();
     private PreparedStatement pstmt;
     private ResultSet rs;
+    private EmpDTO emp = new EmpDTO();
 
-    public AdminServiceImpl() throws SQLException {
+    public AdminServiceImpl(String usrID) throws SQLException {
+    	emp.setUsrID(usrID);
     }
 
     @Override
-    public void adminMenu() {
-    	
+    public void adminMenu() throws IOException {
+    	while(true) {
+    		BW.write("\n======================================================================\n");
+    		BW.write("|\t\t     임직원근태관리 관리자 메뉴\t\t\t     |\n");
+    		BW.write("======================================================================\n");
+    		BW.write("|\t    1. 직원관리\t\t   |\t        2. 출근관리\t     |\n");
+    		BW.write("======================================================================\n");
+    		BW.write("|\t    3. 휴가관리\t\t   |\t        4. 급여관리\t     |\n");
+    		BW.write("======================================================================\n");
+    		BW.write("|\t    5. 부서관리\t\t   |\t        6. 직급관리\t     |\n");
+    		BW.write("======================================================================\n");
+    		BW.write("|\t\t원하는 기능을 선택하세요.(0번 : 종료)\t\t     |\n");
+            BW.write("======================================================================\n");
+    		BW.flush();
+    		
+    		
+    	}
     }
 
 	@Override
